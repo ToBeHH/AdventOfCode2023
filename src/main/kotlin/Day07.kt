ed8f61ad9b932d4580c4bfd7a7c58008bfd8afca
Tobias Schulz-Hess <tobias.schulz-hess@bexapp.de>
2023-12-08 00:27:24 +0100

    Introduced common base class to remove code duplication but actually it made the code just longer and harder to read

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -26,96 +26,21 @@ class Day07(fileName: String) : BaseDay(fileName) {
         return Game2.readGames(lines)
     }
 
-    data class Game(
-        val cards: List<Card>,
-        val bid: Int
-    ) : Comparable<Game> {
-        enum class Card(val char: Char) {
-            A('A'), K('K'), Q('Q'), J('J'), T('T'),
-            NINE('9'), EIGHT('8'), SEVEN('7'), SIX('6'),
-            FIVE('5'), FOUR('4'), THREE('3'), TWO('2');
+    interface CommonCard : Comparable<CommonCard> {
+        val card: Char
 
-            companion object {
-                fun getCard(name: Char) = Card.entries.find { it.char == name }
-            }
-        }
-        enum class Category {
-            FIVE_OF_A_KIND,
-            FOUR_OF_A_KIND,
-            FULL_HOUSE,
-            THREE_OF_A_KIND,
-            TWO_PAIRS,
-            ONE_PAIR,
-            HIGH_CARD
-        }
+        fun getCards(): String
 
-        /**
-         * Compare the game with another game
-         */
-        override fun compareTo(other: Game): Int {
-            return if (this.categorize() == other.categorize()) {
-                for(i in 0..4) {
-                    if (this.cards[i] != other.cards[i]) {
-                        return this.cards[i].compareTo(other.cards[i])
-                    }
-                }
-                return 0
-            } else {
-                this.categorize().compareTo(other.categorize())
-            }
-        }
-
-        /**
-         * Categorize the cards similar to poker
-         */
-        fun categorize(): Category {
-            if (cards.groupBy { it }.filter { it.value.size == 5 }.size == 1) {
-                return Category.FIVE_OF_A_KIND
-            }
-            if (cards.groupBy { it }.filter { it.value.size == 4 }.size == 1) {
-                return Category.FOUR_OF_A_KIND
-            }
-            if (cards.groupBy { it }.filter { it.value.size == 3 }.size == 1 &&
-                cards.groupBy { it }.filter { it.value.size == 2 }.size == 1) {
-                return Category.FULL_HOUSE
-            }
-            if (cards.groupBy { it }.filter { it.value.size == 3 }.size == 1) {
-                return Category.THREE_OF_A_KIND
-            }
-            if (cards.groupBy { it }.filter { it.value.size == 2 }.size == 2) {
-                return Category.TWO_PAIRS
-            }
-            if (cards.groupBy { it }.filter { it.value.size == 2 }.size == 1) {
-                return Category.ONE_PAIR
-            }
-            return Category.HIGH_CARD
-        }
-
-        /**
-         * Factory to read the game from a file
-         */
-        companion object Factory {
-            fun readGames(lines: List<String>): List<Game> {
-                return lines.map { line ->
-                    Game(line.split(" ")[0].toCharArray().toList().map { c -> Card.getCard(c)!! },
-                         line.split(" ")[1].toInt()) }
-            }
+        override fun compareTo(other: CommonCard): Int {
+            return getCards().indexOf(card).compareTo(getCards().indexOf(other.card))
         }
     }
 
-    data class Game2(
-        val cards: List<Card>,
-        val bid: Int
-    ) : Comparable<Game2> {
-        enum class Card(val char: Char) {
-            A('A'), K('K'), Q('Q'), T('T'),
-            NINE('9'), EIGHT('8'), SEVEN('7'), SIX('6'),
-            FIVE('5'), FOUR('4'), THREE('3'), TWO('2'), J('J');
+    interface CommonGame : Comparable<CommonGame> {
+        val cards: List<CommonCard>
 
-            companion object {
-                fun getCard(name: Char) = Card.entries.find { it.char == name }
-            }
-        }
+        val bid: Int
+
         enum class Category {
             FIVE_OF_A_KIND,
             FOUR_OF_A_KIND,
@@ -126,14 +51,14 @@ class Day07(fileName: String) : BaseDay(fileName) {
             HIGH_CARD
         }
 
-        /**
-         * Compare the game with another game
-         */
-        override fun compareTo(other: Game2): Int {
+        fun categorize(): Category
+        fun getCard(index: Int): CommonCard
+
+        override fun compareTo(other: CommonGame): Int {
             return if (this.categorize() == other.categorize()) {
-                for(i in 0..4) {
-                    if (this.cards[i] != other.cards[i]) {
-                        return this.cards[i].compareTo(other.cards[i])
+                for (i in 0..4) {
+                    if (this.getCard(i) != other.getCard(i)) {
+                        return this.getCard(i).compareTo(other.getCard(i))
                     }
                 }
                 return 0
@@ -142,31 +67,7 @@ class Day07(fileName: String) : BaseDay(fileName) {
             }
         }
 
-        /**
-         * Categorize the cards similar to poker, using J as a Joker
-         */
-        fun categorize(): Category {
-            if (cards.contains(Card.J)) {
-                val possibleCategories = mutableListOf<Category>()
-                val grouped = cards.groupBy { it }
-                for (i in grouped.keys) {
-                    if (i != Card.J) {
-                        val newCards = cards.map { if (it == Card.J) i else it }
-                        val cat = category(newCards.groupBy { it })
-                        possibleCategories.add(cat)
-                    }
-                }
-                if (possibleCategories.isEmpty()) {
-                    return Category.FIVE_OF_A_KIND
-                }
-                // actually min is giving the highest possible category
-                return possibleCategories.min()
-            }
-
-            return category(cards.groupBy { it })
-        }
-
-        private fun category(grouped: Map<Card, List<Card>>): Category {
+        fun category(grouped: Map<Char, List<CommonCard>>): Category {
             if (grouped.filter { it.value.size == 5 }.size == 1) {
                 return Category.FIVE_OF_A_KIND
             }
@@ -189,6 +90,135 @@ class Day07(fileName: String) : BaseDay(fileName) {
             }
             return Category.HIGH_CARD
         }
+    }
+
+    class Game(
+        override val cards: List<Card>,
+        override val bid: Int
+    ) : CommonGame {
+        class Card(char: Char) : CommonCard {
+            override val card: Char = char
+
+            override fun getCards(): String {
+                return "AKQJT98765432"
+            }
+
+            override fun equals(other: Any?): Boolean {
+                if (other == null) return false
+                if (other !is Card) return false
+                return card == other.card
+            }
+
+            override fun hashCode(): Int {
+                return card.hashCode()
+            }
+
+            override fun toString(): String {
+                return "Card($card)"
+            }
+
+            companion object {
+                fun getCard(name: Char) = Card(name)
+            }
+        }
+
+        override fun getCard(index: Int): Card {
+            return cards[index]
+        }
+
+        /**
+         * Categorize the cards similar to poker
+         */
+        override fun categorize(): CommonGame.Category {
+            return category(cards.groupBy { it.card })
+        }
+
+        /**
+         * Factory to read the game from a file
+         */
+        companion object Factory {
+            fun readGames(lines: List<String>): List<Game> {
+                return lines.map { line ->
+                    Game(
+                        line.split(" ")[0].toCharArray().toList().map { c -> Card.getCard(c) },
+                        line.split(" ")[1].toInt()
+                    )
+                }
+            }
+        }
+
+        override fun equals(other: Any?): Boolean {
+            if (other == null) return false
+            if (other !is Game) return false
+            return cards == other.cards && bid == other.bid
+        }
+
+        override fun hashCode(): Int {
+            return cards.sumOf { it.card.hashCode() } + bid.hashCode()
+        }
+
+        override fun toString(): String {
+            return "Game(${cards.map { it.card }.joinToString("")}, $bid)"
+        }
+    }
+
+    data class Game2(
+        override val cards: List<Card>,
+        override val bid: Int
+    ) : CommonGame {
+        class Card(char: Char) : CommonCard {
+            override val card: Char = char
+
+            override fun getCards(): String {
+                return "AKQT98765432J"
+            }
+
+            override fun toString(): String {
+                return "Card2($card)"
+            }
+
+            override fun equals(other: Any?): Boolean {
+                if (other == null) return false
+                if (other !is Card) return false
+                return card == other.card
+            }
+
+            override fun hashCode(): Int {
+                return card.hashCode()
+            }
+
+            companion object {
+                fun getCard(name: Char) = Card(name)
+            }
+        }
+
+        override fun getCard(index: Int): Card {
+            return cards[index]
+        }
+
+        /**
+         * Categorize the cards similar to poker, using J as a Joker
+         */
+        override fun categorize(): CommonGame.Category {
+            if (cards.contains(Card('J'))) {
+                val possibleCategories = mutableListOf<CommonGame.Category>()
+                val grouped = cards.groupBy { it }
+                for (i in grouped.keys) {
+                    if (i != Card('J')) {
+                        val newCards = cards.map { if (it == Card('J')) i else it }
+                        val cat = category(newCards.groupBy { it.card })
+                        possibleCategories.add(cat)
+                    }
+                }
+                if (possibleCategories.isEmpty()) {
+                    return CommonGame.Category.FIVE_OF_A_KIND
+                }
+                // actually min is giving the highest possible category
+                return possibleCategories.min()
+            }
+
+            return category(cards.groupBy { it.card })
+        }
 
         /**
          * Factory to read the game from a file
@@ -196,9 +226,26 @@ class Day07(fileName: String) : BaseDay(fileName) {
         companion object Factory {
             fun readGames(lines: List<String>): List<Game2> {
                 return lines.map { line ->
-                    Game2(line.split(" ")[0].toCharArray().toList().map { c -> Card.getCard(c)!! },
-                         line.split(" ")[1].toInt()) }
+                    Game2(
+                        line.split(" ")[0].toCharArray().toList().map { c -> Card.getCard(c) },
+                        line.split(" ")[1].toInt()
+                    )
+                }
             }
+        }
+
+        override fun equals(other: Any?): Boolean {
+            if (other == null) return false
+            if (other !is Game2) return false
+            return cards == other.cards && bid == other.bid
+        }
+
+        override fun hashCode(): Int {
+            return cards.sumOf { it.card.hashCode() } + bid.hashCode()
+        }
+
+        override fun toString(): String {
+            return "Game2(${cards.map { it.card }.joinToString("")}, $bid)"
         }
     }
 }
--- src/test/kotlin/Day07Test.kt
+++ src/test/kotlin/Day07Test.kt
@@ -7,20 +7,26 @@ class Day07Test {
         val d = Day07("day07sample.txt")
         val games = d.readGames()
         assertEquals(5, games.size)
-        assertEquals(Day07.Game(listOf(Day07.Game.Card.THREE,
-            Day07.Game.Card.TWO,
-            Day07.Game.Card.T,Day07.Game.Card.THREE,Day07.Game.Card.K), 765), games[0])
+        assertEquals(
+            Day07.Game(
+                listOf(
+                    Day07.Game.Card('3'),
+                    Day07.Game.Card('2'),
+                    Day07.Game.Card('T'), Day07.Game.Card('3'), Day07.Game.Card('K')
+                ), 765
+            ), games[0]
+        )
     }
 
     @org.junit.jupiter.api.Test
     fun testCategorize() {
         val d = Day07("day07sample.txt")
         val games = d.readGames()
-        assertEquals(Day07.Game.Category.ONE_PAIR, games[0].categorize())
-        assertEquals(Day07.Game.Category.THREE_OF_A_KIND, games[1].categorize())
-        assertEquals(Day07.Game.Category.TWO_PAIRS, games[2].categorize())
-        assertEquals(Day07.Game.Category.TWO_PAIRS, games[3].categorize())
-        assertEquals(Day07.Game.Category.THREE_OF_A_KIND, games[4].categorize())
+        assertEquals(Day07.CommonGame.Category.ONE_PAIR, games[0].categorize())
+        assertEquals(Day07.CommonGame.Category.THREE_OF_A_KIND, games[1].categorize())
+        assertEquals(Day07.CommonGame.Category.TWO_PAIRS, games[2].categorize())
+        assertEquals(Day07.CommonGame.Category.TWO_PAIRS, games[3].categorize())
+        assertEquals(Day07.CommonGame.Category.THREE_OF_A_KIND, games[4].categorize())
     }
 
     @org.junit.jupiter.api.Test
@@ -29,15 +35,27 @@ class Day07Test {
         val games = d.readGames()
         assertEquals(2, games[0].compareTo(games[1]))
         assertEquals(-1, games[1].compareTo(games[2]))
-        assertEquals(-3, games[2].compareTo(games[3]))
+        assertEquals(-1, games[2].compareTo(games[3]))
 
-        var sorted = games.sorted().reversed()
-        assertEquals(Day07.Game(listOf(Day07.Game.Card.THREE,
-            Day07.Game.Card.TWO,
-            Day07.Game.Card.T,Day07.Game.Card.THREE,Day07.Game.Card.K), 765), sorted[0])
-        assertEquals(Day07.Game(listOf(Day07.Game.Card.Q,
-            Day07.Game.Card.Q,
-            Day07.Game.Card.Q,Day07.Game.Card.J,Day07.Game.Card.A), 483), sorted[4])
+        val sorted = games.sorted().reversed()
+        assertEquals(
+            Day07.Game(
+                listOf(
+                    Day07.Game.Card('3'),
+                    Day07.Game.Card('2'),
+                    Day07.Game.Card('T'), Day07.Game.Card('3'), Day07.Game.Card('K')
+                ), 765
+            ), sorted[0]
+        )
+        assertEquals(
+            Day07.Game(
+                listOf(
+                    Day07.Game.Card('Q'),
+                    Day07.Game.Card('Q'),
+                    Day07.Game.Card('Q'), Day07.Game.Card('J'), Day07.Game.Card('A')
+                ), 483
+            ), sorted[4]
+        )
     }
 
     @org.junit.jupiter.api.Test
@@ -51,11 +69,11 @@ class Day07Test {
     fun testCategorize2() {
         val d = Day07("day07sample.txt")
         val games = d.readGames2()
-        assertEquals(Day07.Game2.Category.ONE_PAIR, games[0].categorize())
-        assertEquals(Day07.Game2.Category.FOUR_OF_A_KIND, games[1].categorize())
-        assertEquals(Day07.Game2.Category.TWO_PAIRS, games[2].categorize())
-        assertEquals(Day07.Game2.Category.FOUR_OF_A_KIND, games[3].categorize())
-        assertEquals(Day07.Game2.Category.FOUR_OF_A_KIND, games[4].categorize())
+        assertEquals(Day07.CommonGame.Category.ONE_PAIR, games[0].categorize())
+        assertEquals(Day07.CommonGame.Category.FOUR_OF_A_KIND, games[1].categorize())
+        assertEquals(Day07.CommonGame.Category.TWO_PAIRS, games[2].categorize())
+        assertEquals(Day07.CommonGame.Category.FOUR_OF_A_KIND, games[3].categorize())
+        assertEquals(Day07.CommonGame.Category.FOUR_OF_A_KIND, games[4].categorize())
     }
 
     @org.junit.jupiter.api.Test
